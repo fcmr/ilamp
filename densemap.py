8f0e2e20d5ec987efd16c9c809ff03bf64fee9e9
fcmr <caiomr@proteina.vision.ime.usp.br>
2018-04-20 11:36:43 -0300

    removing the old code now that it is commited. now every cell has at least SAMPLE_PER_CELL points in it.

--- densemap.py
+++ densemap.py
@@ -33,85 +33,6 @@ def SampleSquare(num_samples, limits):
         pts.append((x,y))
     return pts
 
-def SparseMapOld(X, proj, clf):
-    # limits of the projection
-    xmin = np.min(proj[:, 0])
-    xmax = np.max(proj[:, 0])
-    ymin = np.min(proj[:, 1])
-    ymax = np.max(proj[:, 1])
-    x_intervals = np.linspace(xmin - 1e-5, xmax + 1e-5, num=GRID_SIZE + 1)
-    y_intervals = np.linspace(ymin - 1e-5, ymax + 1e-5, num=GRID_SIZE + 1)
-    grid = np.zeros((GRID_SIZE, GRID_SIZE))
-
-    smap = np.ones((GRID_SIZE, GRID_SIZE, 4))
-    num_empty = 0
-    empty_cells = []
-
-    # old sparse map
-    for i in range(GRID_SIZE):
-        idx_i = np.logical_and(proj[:, 0] >= x_intervals[i], proj[:, 0] < x_intervals[i + 1])
-        for j in range(GRID_SIZE):
-            idx_j = np.logical_and(proj[:, 1] >= y_intervals[j], proj[:, 1] < y_intervals[j + 1])
-            idx = np.logical_and(idx_i, idx_j)
-            proj_sub = proj[idx]
-            X_sub = X[idx]
-
-            if proj_sub.shape[0] == 0:
-                # no samples in this tile, mark it as -1 
-                grid[j, i] = -1 
-                num_empty += 1
-                empty_cells.append((j, i))
-            else:
-                # get all samples that projected to that point and feed them to the
-                # classifier
-                labels = clf.predict(X_sub)
-                num_zeros = np.sum(labels == 0)
-                num_ones = np.sum(labels == 1)
-                #print("cell ", i, ";", j, ": ", num_zeros, ";", num_ones)
-
-                # we want to quantify if we have more ones than zeros (or more
-                # zeros than ones):
-                # num_ones == num_zeros -> 0.5
-                # all num_ones -> 1.0
-                # all num_zeros -> 0.0
-                # something like this should work:
-                # 0.5 + (num_ones - num_zeros)/2*(num_ones + num_zeros)
-                grid[j, i] = 0.5 + (num_ones - num_zeros)/(2*(num_ones + num_zeros))
-
-                alpha = abs(num_ones - num_zeros)/(num_ones + num_zeros)
-                if num_ones > num_zeros:
-                    smap[j, i] = np.array([1.0, 0.0, 0.0, alpha])
-                else:
-                    smap[j, i] = np.array([0.0, 0.0, 1.0, alpha])
-
-    print("\tNumber of empty (blank) cells: ", num_empty)
-    return smap, empty_cells
-
-def DenseMapOld(X, proj, sparse_map, empty_cells, clf):
-    print("Filling empty cells...")
-    start = time()
-    xmin = np.min(proj[:, 0])
-    xmax = np.max(proj[:, 0])
-    ymin = np.min(proj[:, 1])
-    ymax = np.max(proj[:, 1])
-    x_intervals = np.linspace(xmin - 1e-5, xmax + 1e-5, num=GRID_SIZE + 1)
-    y_intervals = np.linspace(ymin - 1e-5, ymax + 1e-5, num=GRID_SIZE + 1)
-
-    #dmap = np.ones((GRID_SIZE, GRID_SIZE, 4))
-    dmap = np.copy(sparse_map)
-    for (j, i) in empty_cells:
-        # generate sample with iLAMP at the center of the cell
-        x_center = (x_intervals[i] + x_intervals[i + 1])*0.5
-        y_center = (y_intervals[j] + y_intervals[j + 1])*0.5
-        new_sample = lamp.ilamp(X, proj, np.array([x_center, y_center]))
-        label = clf.predict([new_sample])[0]
-        if label == 1:
-            dmap[j, i] = np.array([1.0, 0.65, 0.0, 1.0])
-        else:
-            dmap[j, i] = np.array([0.0, 0.65, 1.0, 1.0])
-    print("\tFinished filling empty cells: ", time() - start)
-    return dmap
-
 def SparseMap(X, cells, clf):
     gridsz = len(cells)
     smap = np.ones((gridsz, gridsz, 4))
@@ -257,34 +178,6 @@ def main():
     plt.savefig("dense_map.png")
     plt.clf()
     print("\tFinished plotting dense grid: ", time() - start)
-
-    # FIXME: old code, check if it is faster now and remove it
-    #print("Creating sparse grid old...")
-    #start = time()
-    #sparse_map, empty_cells = SparseMapOld(X, proj, clf)
-    #print("\tFinished computing sparse grid: ", time() - start)
-
-    #print("Plotting sparse grid...")
-    #start = time()
-    #tmp_sparse = np.flip(sparse_map, axis=0)
-    #plt.imshow(tmp_sparse, interpolation='none')
-    #plt.savefig("sparse_map.png")
-    #plt.clf()
-    #print("\tFinished plotting sparse grid: ", time() - start)
-    
-    #print("Creating dense grid old...")
-    #start = time()
-    #dense_map = DenseMapOld(X, proj, sparse_map, empty_cells, clf)
-    #print("\tFinished computing dense grid: ", time() - start)
-
-    #print("Plotting dense map...")
-    #start = time()
-    #tmp_dense = np.flip(dense_map, axis=0)
-    #plt.imshow(tmp_dense, interpolation='none')
-    #plt.savefig("densemap.png")
-    #plt.clf()
-    #print("\tFinished plotting densemap: ", time() - start)
-    ##plt.show()
 
 if __name__ == "__main__":
     main()
