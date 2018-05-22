eebcb78ecf2fb26f1613e40974d512b3f23e4320
fcmr <caiomr@proteina.vision.ime.usp.br>
2018-05-22 15:48:20 -0300

    small fix in the transferfunc, reading other datasets and added colormaps for 3 and 7 class

--- densemap.py
+++ densemap.py
@@ -2,6 +2,7 @@ import numpy as np
 import lamp
 
 from sklearn import linear_model
+from sklearn import preprocessing 
 
 from time import time
 
@@ -10,12 +11,72 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
 
-GRID_SIZE = 50
-SAMPLES_PER_CELL = 5
+import sys
+
+GRID_SIZE = 200 
+
+NUM_CLASSES = 0
+COLORS = [#0 class:
+          [], 
+          #1 class:
+          [],
+          #2 classes:
+          [[0.0, 0.0, 1.0, 0.5], [1.0, 0.0, 0.0, 0.5]],
+          #3 classes:
+          [[0.0, 0.0, 1.0, 0.5], [0.0, 1.0, 0.0, 0.5], [1.0, 0.0, 0.0, 0.5]],
+          #4 classes:
+          [],
+          #5 classes:
+          [],
+          #6 classes:
+          [],
+          #7 classes:
+          [(0.121, 0.466, 0.705),
+           (1.0, 0.498, 0.054),
+           (0.172, 0.627, 0.172),
+           (0.839, 0.152, 0.156),
+           (0.580, 0.403, 0.741),
+           (0.549, 0.337, 0.294),
+           (0.890, 0.466, 0.760)]
+         ]
+CMAP_ORIG = [#0 class:
+             [],
+             #1 class:
+             [],
+             #2 classes:
+             [0.66, 0.0],
+             #3 classes:
+             [0.66, 0.33, 0.0],
+             #4 classes:
+             [],
+             #5 classes:
+             [],
+             #6 classes:
+             [],
+             #7 classes:
+             np.linspace(0.86, 0.0, 7)
+           ]
+CMAP_SYN = [#0 class:
+            [],
+            #1 class
+            [],
+            #2 classes:
+            [0.558, 0.108],
+            #3 classes:
+            [0.558, 0.194, 0.108],
+            #4 classes:
+            [],
+            #5 classes:
+            [],
+            #6 classes:
+            [],
+            np.linspace(0.79, 0.108, 7)
+           ]
 
 np.random.seed(0)
 
 def S(c, d):
+    # lerp(lerp(Sb, Sa, c), lerp(Sd, Sc, c), d)
     if d <= 0.5:
         # Sa: dark red rgb(0.372, 0.098, 0.145) - hsv(0.972, 1.0, 0.5)
         Sa = 1.0
@@ -59,18 +120,50 @@ def V(c, d):
         Vd = 1.0
         return (Va*c + Vb*(1.0 - c))*(2.0 - 2.0*d) + (Vd*c + Vc*(1.0 - c))*(2*d - 1)
 
+def TransferFuncHSV(hsv, k):
+    a = 0.05
+    b = 0.95
+    new_img = np.copy(hsv)
+    new_img[:, :, 2] = (a + b*new_img[:, :, 2])**k
+    return new_img
+
+def TransferFunc(rgb, hsv, k):
+    a = 0.1
+    b = 0.9
+    v = np.repeat(hsv[:, :, 2], 3).reshape(hsv.shape)
+    new_rgb = np.copy(rgb)
+    new_rgb = new_rgb*((a + b*v)**k)
+    return new_rgb
+
 # simple plot_embeddings: work on something fancier later
 def PlotProj(X, colors):
     plt.axes().set_aspect('equal')
-    plt.scatter(X[:, 0], X[:, 1], color=colors)
+    plt.scatter(X[:, 0], X[:, 1], color=colors, s=10.0)
     plt.savefig("dataset_projection.png")
     plt.clf()
     #plt.show()
 
+# TODO: train/test split?
 def LoadZeroOneData():
     X = np.load("data/zero_one.npy")
     y = np.load("data/zero_one_label.npy")
     return X, y
+
+def LoadWineData():
+    data = np.loadtxt("data/wine/wine.data", delimiter=",")
+    y = data[:, 0].astype(int) - 1
+    X = data[:, 1:]
+    # normalizes X
+    X = preprocessing.MinMaxScaler().fit_transform(X)
+    return X, y
+
+def LoadSegmentationData():
+    data = np.loadtxt("data/segmentation/segmentation.txt", delimiter=",", comments='#')
+    y = data[:, 0].astype(int)
+    X = data[:, 1:]
+    X = preprocessing.MinMaxScaler().fit_transform(X)
+    return X, y
+
 
 def SampleSquare(num_samples, limits):
     pts = []
@@ -139,6 +232,7 @@ def SparseMapHSV(X, cells, clf):
             if num_pts > max_pts:
                 max_pts = num_pts
 
+
     # Construct the sparse grid based on the list of indices for each cell
     for row in range(gridsz):
         for col in range(gridsz):
@@ -175,15 +269,19 @@ def SparseMapHSVNew(X, cells, clf, num_per_cell=1):
             if num_pts > max_pts:
                 max_pts = num_pts
 
+    print("\t\tMAX_PTS: ", max_pts)
+
     # color map: hue values for the two-class problem
-    cmap = [0.65, 0.0]
+    #cmap = [0.65, 0.0]
+    cmap = CMAP_ORIG[NUM_CLASSES]
 
     # Construct the sparse grid based on the list of indices for each cell
     for row in range(gridsz):
         for col in range(gridsz):
             if len(cells[row][col]) < num_per_cell:
                 # white cell
-                smap[row, col, 2] = 1.0
+                #smap[row, col, 2] = 1.0
+                smap[row, col] = np.array([0.0, 0.0, 0.2])
                 continue
 
             X_sub = X[cells[row][col]]
@@ -203,21 +301,6 @@ def SparseMapHSVNew(X, cells, clf, num_per_cell=1):
             s = S(c, d)
             v = V(c, d)
             smap[row, col] = np.array([hue, s, v])
-
-
-            #saturation = num_winning/num_pts
-            #hsv = np.array([hue, saturation, 1.0])
-            #hsv_white = np.array([0.0, 0.0, 1.0])
-            #smap[row, col] = (1.0 - d)*hsv_white + d*hsv
-
-            #if num_ones > num_zeros:
-            #    hue = 0.0
-            #    luminance = num_ones/(num_ones + num_zeros)
-            #    smap[row, col] = np.array([hue, saturation, luminance])
-            #else:
-            #    hue = 0.65
-            #    luminance = num_zeros/(num_ones + num_zeros)
-            #    smap[row, col] = np.array([hue, saturation, luminance])
     return smap
 
 
@@ -374,19 +457,21 @@ def DenseMapHSVNew(X, proj, smap, cells, clf, num_per_cell=1):
             if num_pts > max_pts:
                 max_pts = num_pts
 
+    if max_pts < num_per_cell:
+        max_pts = num_per_cell
+        print("\t\t\tMAX_PTS == N")
 
-    # color map: hue values for the two-class problem
-    cmap = [0.558, 0.108]
+    cmap = CMAP_SYN[NUM_CLASSES]
     for row in range(gridsz):
         for col in range(gridsz):
             if len(cells[row][col]) >= num_per_cell:
                 continue
 
+            #FIXME: decide if original hue values should be kept or not
             if len(cells[row][col]) != 0:
-                cmap = [0.65, 0.0]
+                cmap = CMAP_ORIG[NUM_CLASSES]
             else:
-                cmap = [0.558, 0.108]
-
+                cmap = CMAP_SYN[NUM_CLASSES]
 
             X_sub = [x for x in X[cells[row][col]]]
             limits = [x_intrvls[col], y_intrvls[row], x_intrvls[col + 1], y_intrvls[row + 1]]
@@ -433,8 +518,17 @@ def main():
     # 1. loads a 2-class dataset
     print("Loading data...")
     start = time()
+    # Toy dataset composed of digits 0 and 1 from MNIST
     X, y = LoadZeroOneData() 
+    #X, y = LoadWineData() 
+    #X, y = LoadSegmentationData()
     print("\tLoading finished: ", time() - start)
+
+    # count the number of classes:
+    global NUM_CLASSES
+    NUM_CLASSES = len(np.bincount(y))
+    print("NUM_CLASSES: ", NUM_CLASSES)
+    colors = COLORS[NUM_CLASSES]
 
     # 2. Trains a linear classifier, just for testing
     print("Training classifier...")
@@ -450,13 +544,15 @@ def main():
     proj = lamp.lamp2d(X)
     print("\tProjection finished: ", time() - start)
     y_pred = clf.predict(X)
-    colors = ['blue', 'red']
+    #colors = ['blue', 'green', 'red']
+    #colors = [[0.0, 0.0, 1.0, 0.5], [0.0, 1.0, 0.0, 0.5], [1.0, 0.0, 0.0, 0.5]]
     proj_colors = [colors[i] for i in y_pred]
 
     print("Plotting LAMP projection..")
     start = time()
     PlotProj(proj, proj_colors)
     print("\tPlotting finished: ", time() - start)
+    #sys.exit(0)
 
     # cells will store the indices of the points that fall inside each cell
     # of the grid
@@ -527,11 +623,39 @@ def main():
     print("Plotting sparse grid...")
     start = time()
     tmp_sparse = np.flip(sparse_map, axis=0)
-    plt.imshow(hsv_to_rgb(tmp_sparse), interpolation='none')
-    plt.savefig("new_hsv/sparse_map_hsv_" + str(1) + "_samples.png")
+    rgb_img = hsv_to_rgb(tmp_sparse)
+    plt.imshow(rgb_img, interpolation='none')
+    fig_title = "{}x{} SparseMap".format(GRID_SIZE, GRID_SIZE)
+    plt.title(fig_title)
+    fig_name = "new_hsv_k/sparse_map_hsv_" + str(1) + "_samples.png"
+    plt.savefig(fig_name)
     plt.clf()
     print("\tFinished plotting sparse grid: ", time() - start)
-    for i in [1, 5, 10, 15]:
+
+    #ks = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9, 1.1]
+    ks = [0.4, 0.5, 0.6, 0.7, 0.9, 1.1]
+    for k in ks:
+        print("Plotting sparse grid...")
+        start = time()
+        tmp_sparse = np.flip(sparse_map, axis=0)
+        # pass V through the transfer function
+        tmp_sparse = TransferFuncHSV(tmp_sparse, k)
+        rgb_img = hsv_to_rgb(tmp_sparse)
+        # FIXME: pass only V through the transfer function?
+        #rgb_img = TransferFunc(rgb_img, tmp_sparse, k)
+        plt.imshow(rgb_img, interpolation='none')
+        fig_title = "{}x{} SparseMap - K = {}".format(GRID_SIZE, GRID_SIZE, k)
+        plt.title(fig_title)
+        fig_name = "new_hsv_k/sparse_map_hsv_{}_samples_{}.png".format(1, k)
+        plt.savefig(fig_name)
+        plt.clf()
+        print("\tFinished plotting sparse grid: ", time() - start)
+
+
+
+    #for i in [1, 5, 10, 15]:
+    #for i in [1, 2, 3]:
+    for i in [1, 5, 10]:
         print("Creating dense grid hsv " + str(i) + " ...")
         start = time()
         dense_map = DenseMapHSVNew(X, proj, sparse_map, cells, clf, i)
@@ -541,9 +665,28 @@ def main():
         start = time()
         tmp_dense = np.flip(dense_map, axis=0)
         plt.imshow(hsv_to_rgb(tmp_dense), interpolation='none')
-        plt.savefig("new_hsv/dense_map_hsv_" + str(i) + "_samples.png")
+        fig_title = "{}x{} DenseMap ({} samples)".format(GRID_SIZE, GRID_SIZE, i)
+        plt.title(fig_title)
+        fig_name = "new_hsv_k/dense_map_hsv_" + str(i) + "_samples.png"
+        plt.savefig(fig_name)
         plt.clf()
         print("\tFinished plotting dense grid: ", time() - start)
+
+        for k in ks:
+            print("Plotting dense grid...")
+            start = time()
+            tmp_dense = np.flip(dense_map, axis=0)
+            #tmp_dense[:, :, 2] = TransferFunc(tmp_dense[:, :, 2], k)
+            tmp_dense = TransferFuncHSV(tmp_dense, k)
+            rgb_img = hsv_to_rgb(tmp_dense)
+            #rgb_img = TransferFunc(rgb_img, tmp_dense, k)
+            plt.imshow(rgb_img, interpolation='none')
+            fig_title = "{}x{} DenseMap ({} samples, k = {})".format(GRID_SIZE, GRID_SIZE, i, k)
+            plt.title(fig_title)
+            fig_name = "new_hsv_k/dense_map_hsv_{}_samples_{}.png".format(i, k)
+            plt.savefig(fig_name)
+            plt.clf()
+            print("\tFinished plotting dense grid: ", time() - start)
 
 if __name__ == "__main__":
     main()
